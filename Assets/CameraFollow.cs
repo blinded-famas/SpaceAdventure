0dc70f7230483278b8139e3afda9de3657a9321e
blinded-famas <alex.blinded@gmail.com>
2019-09-18 11:18:41 +0300

    initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetToFollow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(targetToFollow.position.y, 0f, 10f), transform.position.z);
+    }
+}
